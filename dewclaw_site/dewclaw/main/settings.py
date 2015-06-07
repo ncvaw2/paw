596bc4753246987bcf6ed0a41c0ba98f66110b8e
ncvaw2 <ncvaw2@gmail.com>
2015-06-06 22:43:33 -0400

    more mods

--- dewclaw_site/dewclaw/main/settings.py
+++ dewclaw_site/dewclaw/main/settings.py
@@ -44,11 +44,7 @@ WSGI_APPLICATION = 'main.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-		'ENGINE': 'django.db.backends.sqlite3',
-	 	'NAME': 'project.db', 'HOST': 'localhost', 'USER': '', 'PASSWORD': '', 'PORT': ''}
-}
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
